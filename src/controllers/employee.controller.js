e6c00f636293fb6edb7962a5465039b4b68f3889
Marciano Moreno <marciano_moreno@outlook.com>
2022-01-30 14:29:35 -0600

    Standarizing logging in controller

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -2,7 +2,7 @@
 const Employee = require('../models/employee.model');
 exports.findAll = function(req, res) {
 Employee.findAll(function(err, employee) {
-  console.log('controller')
+  console.log('controller:findAll')
   if (err)
   res.send(err);
   console.log('res', employee);
@@ -11,48 +11,52 @@ Employee.findAll(function(err, employee) {
 };
 exports.create = function(req, res) {
 const new_employee = new Employee(req.body);
+console.log('controller:create')
 //handles null error
 if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-  console.log("Employee:create:Missing key");
-  res.status(400).send({ error:true, message: 'Please provide all required field' });
+  console.log('controller:create:Missing key');
+  res.status(400).send({ error:true, message: 'Please provide all required fields.' });
 }else{
 Employee.create(new_employee, function(err, employee) {
   if (err){
     res.status(400).send({ error:true, message: err});
     return;
   }
-  res.json({error:false,message:"Employee added successfully!",data:employee});
+  res.json({error:false,message:'Employee added successfully!',data:employee});
 });
 }
 };
 exports.findById = function(req, res) {
 Employee.findById(req.params.id, function(err, employee) {
+  console.log('controller:findById')
   if (err)
   res.send(err);
   res.json(employee);
 });
 };
 exports.update = function(req, res) {
+  console.log('controller:update')
   if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-    console.log("Employee:update:Missing key");
-    res.status(400).send({ error:true, message: 'Please provide all required field' });
+    console.log('controller:update:Missing key');
+    res.status(400).send({ error:true, message: 'Please provide all required fields' });
   }else{
     Employee.update(req.params.id, new Employee(req.body), function(err, employee) {
    if (err)
    res.send(err);
-   res.json({ error:false, message: 'Employee successfully updated' });
+   res.json({ error:false, message: 'Employee successfully updated.' });
 });
 }
 };
 exports.partialUpdate = function(req, res){
 Employee.partialUpdate(req.params.id, new Employee(req.body), function(err, employee) {
-    
+console.log('controller:partialUpdate')
 });
 };
 exports.delete = function(req, res) {
 Employee.delete( req.params.id, function(err, employee) {
+  console.log('controller:delete')
   if (err)
   res.send(err);
-  res.json({ error:false, message: 'Employee successfully deleted' });
+  res.json({ error:false, message: 'Employee successfully deleted.' });
 });
 };
