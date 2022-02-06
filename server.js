fae68053f5159850e2a41a0b4591ba89b33366be
Marciano Moreno <marciano_moreno@outlook.com>
2022-02-06 17:37:06 -0600

    Added views, home and login routes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+const dotenv = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 // create express app
@@ -6,17 +6,28 @@ const app = express();
 // Setup server port
 const port = process.env.PORT || 5000;
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
 // define a root route
+
+/*
 app.get('/', (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!!!!!!!");
 });
+
+*/
 // Require employee routes
-const employeeRoutes = require('./src/routes/employee.routes')
+const homeRoutes = require('./src/routes/home.routes');
+const employeeRoutes = require('./src/routes/employee.routes');
+const loginRoutes = require('./src/routes/login.routes');
 // using as middleware
-app.use('/api/v1/employees', employeeRoutes)
+app.use('/', homeRoutes);
+app.use('/api/v1/employees', employeeRoutes);
+app.use('/login', loginRoutes);
 // listen for requests
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
